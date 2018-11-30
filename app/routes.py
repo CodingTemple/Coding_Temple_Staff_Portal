23c360553239c593b7756e4494fbcd0ca2cd6c12
derekmhawkins <derekmhawkins@gmail.com>
2018-11-30 00:46:08 -0600

    Will redirect to login screen upon website visit

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
+from flask_login import current_user
 
 @app.route('/')
 def index():
@@ -7,6 +8,8 @@ def index():
   [GET] / \n 
   Default Route
   '''
+  if current_user.is_anonymous:
+    return redirect(url_for('account.login'))
   context = {
     'title': 'Home'
   }
